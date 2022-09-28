095160c92e020638362ee72f8c6ddb4ac800f444
Amrit Pal Singh <amrit220399@gmail.com>
2022-09-28 10:40:13 +0530

    Call Detector Library Added

--- src/Containers/ExampleContainer.js
+++ src/Containers/ExampleContainer.js
@@ -1,102 +1,125 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import {
-  View,
-  ActivityIndicator,
+  Button,
+  Linking,
+  FlatList,
+  StyleSheet,
   Text,
-  TextInput,
+  View,
   TouchableOpacity,
-  ScrollView,
 } from 'react-native'
-import { useDispatch } from 'react-redux'
-import { useTranslation } from 'react-i18next'
-import { Brand } from '@/Components'
+
 import { useTheme } from '@/Hooks'
-import { useLazyFetchOneQuery } from '@/Services/modules/users'
-import { changeTheme } from '@/Store/Theme'
+import { useRef } from 'react'
+import { useState } from 'react'
+import CallDetectorManager from 'react-native-call-detection'
 
 const ExampleContainer = () => {
-  const { t } = useTranslation()
-  const { Common, Fonts, Gutters, Layout } = useTheme()
-  const dispatch = useDispatch()
+  const { Gutters, Layout } = useTheme()
+  const callDetector = useRef()
+  const ds = []
+  const [callStates, setCallStates] = useState([])
 
-  const [userId, setUserId] = useState('9')
-  const [fetchOne, { data, isSuccess, isLoading, isFetching, error }] =
-    useLazyFetchOneQuery()
+  const [dsState, setDsState] = useState(ds)
 
-  useEffect(() => {
-    fetchOne(userId)
-  }, [fetchOne, userId])
+  const startListenerTapped = () => {
+    console.log('Start Listener Called')
+    callDetector.current = new CallDetectorManager(
+      (event, number) => {
+        var updatedCallStates = callStates
+        updatedCallStates.push(event + ' - ' + number)
+        var previousDS = dsState
+        setCallStates(updatedCallStates)
+        setDsState(previousDS.cloneWithRows(updatedCallStates))
+      },
+      true,
+      () => {},
+      {
+        title: 'Phone State Permission',
+        message:
+          'This app needs access to your phone state in order to react and/or to adapt to incoming calls.',
+      },
+    )
+  }
 
-  const onChangeTheme = ({ theme, darkMode }) => {
-    dispatch(changeTheme({ theme, darkMode }))
+  const callFriendTapped = () => {
+    Linking.openURL('tel:9217721412').catch(err => {
+      console.log(err)
+    })
+  }
+
+  const stopListenerTapped = () => {
+    callDetector.current && callDetector.current.dispose()
   }
 
   return (
-    <ScrollView
-      style={Layout.fill}
-      contentContainerStyle={[
-        Layout.fill,
-        Layout.colCenter,
-        Gutters.smallHPadding,
-      ]}
-    >
-      <View style={[[Layout.colCenter, Gutters.smallHPadding]]}>
-        <Brand />
-        {(isLoading || isFetching) && <ActivityIndicator />}
-        {!isSuccess ? (
-          <Text style={Fonts.textRegular}>{error}</Text>
-        ) : (
-          <Text style={Fonts.textRegular}>
-            {t('example.helloUser', { name: data?.name })}
+    <FlatList
+      data={dsState}
+      renderItem={({ item }) => <Text style={styles.callLogs}>{item}</Text>}
+      ListHeaderComponent={
+        <View style={Layout.fill}>
+          <TouchableOpacity onPress={startListenerTapped}>
+            <Text
+              title="Start Listener"
+              color="#841584"
+              style={styles.bottomMargin}
+            >
+              Start Listener
+            </Text>
+          </TouchableOpacity>
+
+          <Text
+            onPress={callFriendTapped}
+            title="Call your Friend"
+            style={styles.bottomMargin}
+          >
+            Call your Friend
           </Text>
-        )}
-      </View>
-      <View
-        style={[
-          Layout.row,
-          Layout.rowHCenter,
-          Gutters.smallHPadding,
-          Gutters.largeVMargin,
-          Common.backgroundPrimary,
-        ]}
-      >
-        <Text style={[Layout.fill, Fonts.textCenter, Fonts.textSmall]}>
-          {t('example.labels.userId')}
-        </Text>
-        <TextInput
-          onChangeText={setUserId}
-          editable={!isLoading}
-          keyboardType={'number-pad'}
-          maxLength={1}
-          value={userId}
-          selectTextOnFocus
-          style={[Layout.fill, Common.textInput]}
-        />
-      </View>
-      <Text style={[Fonts.textRegular, Gutters.smallBMargin]}>DarkMode :</Text>
 
-      <TouchableOpacity
-        style={[Common.button.rounded, Gutters.regularBMargin]}
-        onPress={() => onChangeTheme({ darkMode: null })}
-      >
-        <Text style={Fonts.textRegular}>Auto</Text>
-      </TouchableOpacity>
+          <Text
+            onPress={stopListenerTapped}
+            title="Stop Listener"
+            color="#841584"
+            style={styles.bottomMargin}
+          >
+            Stop Listener
+          </Text>
 
-      <TouchableOpacity
-        style={[Common.button.outlineRounded, Gutters.regularBMargin]}
-        onPress={() => onChangeTheme({ darkMode: true })}
-      >
-        <Text style={Fonts.textRegular}>Dark</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        style={[Common.button.outline, Gutters.regularBMargin]}
-        onPress={() => onChangeTheme({ darkMode: false })}
-      >
-        <Text style={Fonts.textRegular}>Light</Text>
-      </TouchableOpacity>
-    </ScrollView>
+          <Text style={styles.text}>Call State Logs</Text>
+        </View>
+      }
+    />
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 40,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  bottomMargin: {
+    color: '#ffffff',
+    backgroundColor: '#841584',
+    textAlign: 'center',
+    paddingVertical: 15,
+    borderBottomWidth: 0.6,
+    borderBottomColor: '#ffffff',
+  },
+  text: {
+    marginTop: 30,
+    textAlign: 'center',
+    fontSize: 20,
+    color: '#341584',
+  },
+  callLogs: {
+    textAlign: 'center',
+    fontSize: 15,
+    color: '#333333',
+    marginBottom: 5,
+  },
+})
 
 export default ExampleContainer
